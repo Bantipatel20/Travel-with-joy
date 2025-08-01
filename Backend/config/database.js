94e66afed46fad63b765d96f6eae5177a026acef
Bantipatel20 <patelbanti993@gmail.com>
2025-08-01 14:18:45 +0530

    database.js completed

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -4,4 +4,12 @@ const mongoose = require("mongoose");
 
 require("dotenv").config;
 
-exports.connectiondb = mongoose.connect()
+const connectiondb = ()=>{
+    mongoose.connect(Process.env.MONGODB_URL,{
+        useUrlParser:true,
+        useUnifiedTopology:true
+    }).then(()=>console.log("DB connection Successfully"))
+    .catch((err)=>console.log("DB connection Error"))
+}
+
+module.exports = connectiondb;

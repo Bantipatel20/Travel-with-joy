410ac73b8daff7ff9a88d1180b18b6e21a876cd4
Bantipatel20 <patelbanti993@gmail.com>
2025-09-14 12:13:49 +0530

    Update cloudinary.js

--- travel-backend/config/cloudinary.js
+++ travel-backend/config/cloudinary.js
@@ -1,15 +1,14 @@
-import cloudinaryPkg from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const { v2: cloudinary, CloudinaryStorage } = cloudinaryPkg;
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Storage for hotels

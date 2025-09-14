56026d19e868aa5f4c036ed5137aca7c4d4ee986
Bantipatel20 <patelbanti993@gmail.com>
2025-09-14 11:50:01 +0530

    multer setup

--- travel-backend/config/cloudinary.js
+++ travel-backend/config/cloudinary.js
@@ -1,35 +1,37 @@
 import { v2 as cloudinary } from "cloudinary";
-import {CloudinaryStorage} from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "cloudinary";
 import multer from "multer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-const hotelstorage = new CloudinaryStorage({
-    cloudinary,
-    params:{
-        folder:"TMS/hotels",
-        allowed_formats: ["jpg,jpeg,png"],
-        transformation:[{width:800,height:600,crop:"limit"}],
-    },
-})
+// Storage for hotels
+const hotelStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "TMS/hotels",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    transformation: [{ width: 800, height: 600, crop: "limit" }],
+  },
+});
 
-const destinationstorage = new CloudinaryStorage({
-    cloudinary,
-    params: {
+// Storage for destinations
+const destinationStorage = new CloudinaryStorage({
+  cloudinary,
+  params: {
     folder: "TMS/destinations",
     allowed_formats: ["jpg", "jpeg", "png"],
     transformation: [{ width: 1000, height: 700, crop: "limit" }],
-  }
-})
+  },
+});
 
-export const uploadhotel = multer ( {storage:hotelstorage});
-export const uploaddestination = multer({storage:destinationstorage});
+export const uploadHotel = multer({ storage: hotelStorage });
+export const uploadDestination = multer({ storage: destinationStorage });
 
 export default cloudinary;

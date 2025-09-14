3284855fb0ec15d92450e9cb3a4b1b009cc4f18e
Bantipatel20 <patelbanti993@gmail.com>
2025-09-14 12:06:21 +0530

    Update hotel.js

--- travel-backend/routes/hotel.js
+++ travel-backend/routes/hotel.js
@@ -1,14 +1,14 @@
-import { createhotel,getallhotel,deletehotel,updatehotel } from "../controllers/hotel.js";
+import { createhotel, getallhotel, deletehotel, updatehotel } from "../controllers/hotel.js";
 import express from "express";
-import { auth,adminonly } from "../middlewares/auth.js";
-import { uploadhotel } from "../config/cloudinary.js";
-import { hotelValidator } from "../utils/validators.js"
+import { auth, adminonly } from "../middlewares/auth.js";
+import { uploadHotel } from "../config/cloudinary.js";
+import { hotelValidator } from "../utils/validators.js";
 
 const router = express.Router();
 
-router.get('/',getallhotel);
-router.post('/',auth,adminonly,uploadhotel.array('images',5),hotelValidator,createhotel);
-router.put('/:id',auth,adminonly,uploadhotel.array('images',5),hotelValidator,updatehotel);
-router.delete("/:id",auth,adminonly,deletehotel);
+router.get('/', getallhotel);
+router.post('/', auth, adminonly, uploadHotel.array('images', 5), hotelValidator, createhotel);
+router.put('/:id', auth, adminonly, uploadHotel.array('images', 5), hotelValidator, updatehotel);
+router.delete("/:id", auth, adminonly, deletehotel);
 
 export default router;
